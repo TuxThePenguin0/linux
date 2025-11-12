3abf6f26e7e12456481d6fa16cc4091400c11071
Alexander Warnecke <awarnecke002@hotmail.com>
2025-11-12 21:54:47 +1100

    regulator: bq257xx: Fix missing module alias
    The bq257xx-regulator module is not always automatically loaded otherwise.
    
    Signed-off-by: Alexander Warnecke <awarnecke002@hotmail.com>

--- drivers/regulator/bq257xx-regulator.c
+++ drivers/regulator/bq257xx-regulator.c
@@ -184,3 +184,4 @@ module_platform_driver(bq257xx_reg_driver);
 MODULE_DESCRIPTION("bq257xx regulator driver");
 MODULE_AUTHOR("Chris Morgan <macromorgan@hotmail.com>");
 MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:bq257xx-regulator");
